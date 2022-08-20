00e156af7c715f0ed8501d68d5587e6d400fa772
kasturi28 <kasturipansare2818@gmail.com>
2022-08-20 19:44:37 +0530

    Added_Welcome_Message

---
+++ src/workshoproblem/HotelReservation.java
@@ -0,0 +1,11 @@
+package workshoproblem;
+
+public class HotelReservation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Hotel reservation program....");
+
+	}
+
+}
