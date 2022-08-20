3cf9b021ebc5624237a363634794f9305e378f13
kasturi28 <kasturipansare2818@gmail.com>
2022-08-20 19:45:34 +0530

    Refactor_Welcome_Message

--- src/workshoproblem/HotelReservation.java
+++ src/workshoproblem/HotelReservation.java
@@ -1,9 +1,14 @@
 package workshoproblem;
 
 public class HotelReservation {
-
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		
+		/*
+		 * Printing the welcome message
+		 */
 		System.out.println("Welcome to Hotel reservation program....");
 
 	}
